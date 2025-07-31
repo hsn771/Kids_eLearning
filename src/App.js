1dd9044572b8c046d24e7273dc1a0fc19cb09985
ImtiazAhmedArefin <imtiazahmed.arefin@gmail.com>
2025-07-31 13:21:52 +0600

    2nd change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import{BrowserRouter, Route, Routes} from 'react-router';
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Classes from "./pages/Classes";
+import Facilities from "./pages/Facilities";
+import Contact from "./pages/Contact";
+import "./assets/lib/animate/animate.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
+import "./assets/importfile.css";
 
 function App() {
   return (
@@ -9,6 +16,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="/classes" element={<Classes/>}/>
+      <Route path="/facilities" element={<Facilities/>}/>
+      <Route path="/contact" element={<Contact/>}/>
     </Routes>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,34 @@ import {Link,useLocation} from 'react-router';
 function Header() {
     return(
         <>
-        <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5 py-lg-0">
-            <a href="index.html" class="navbar-brand">
-                <h1 class="m-0 text-primary"><i class="fa fa-book-reader me-3"></i>Kider</h1>
+        <nav className="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5 py-lg-0">
+            <a href="index.html" className="navbar-brand">
+                <h1 className="m-0 text-primary"><i className="fa fa-book-reader me-3"></i>Kider</h1>
             </a>
-            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
+            <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <div class="navbar-nav mx-auto">
-                    <Link to="/" class="nav-item nav-link">Home</Link>
-                    {/* <a href="index.html" class="nav-item nav-link active">Home</a> */}
-                    <Link to="/about" class="nav-item nav-link">About Us</Link>
-                    {/* <a href="about.html" class="nav-item nav-link">About Us</a> */}
-                    <a href="classes.html" class="nav-item nav-link">Classes</a>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                        <div class="dropdown-menu rounded-0 rounded-bottom border-0 shadow-sm m-0">
-                            <a href="facility.html" class="dropdown-item">School Facilities</a>
-                            <a href="team.html" class="dropdown-item">Popular Teachers</a>
-                            <a href="call-to-action.html" class="dropdown-item">Become A Teachers</a>
-                            <a href="appointment.html" class="dropdown-item">Make Appointment</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                            <a href="404.html" class="dropdown-item">404 Error</a>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <div className="navbar-nav mx-auto">
+                    <Link to="/" className="nav-item nav-link">Home</Link>
+                    {/* <a href="index.html" className="nav-item nav-link active">Home</a> */}
+                    <Link to="/about" className="nav-item nav-link">About Us</Link>
+                    {/* <a href="about.html" className="nav-item nav-link">About Us</a> */}
+                    <Link to={"/classes"} className="nav-item nav-link">Classes</Link>
+                    <div className="nav-item dropdown">
+                        <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">More</a>
+                        <div className="dropdown-menu rounded-0 rounded-bottom border-0 shadow-sm m-0">
+                            <Link to={"/facilities"} className="dropdown-item">School Facilities</Link>
+                            <a href="team.html" className="dropdown-item">Popular Teachers</a>
+                            <a href="call-to-action.html" className="dropdown-item">Become A Teachers</a>
+                            <a href="appointment.html" className="dropdown-item">Make Appointment</a>
+                            <a href="testimonial.html" className="dropdown-item">Testimonial</a>
+                            <a href="404.html" className="dropdown-item">404 Error</a>
                         </div>
                     </div>
-                    <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+                    <Link to={"/contact"} className="nav-item nav-link">Contact Us</Link>
                 </div>
-                <a href="" class="btn btn-primary rounded-pill px-3 d-none d-lg-block">Join Us<i class="fa fa-arrow-right ms-3"></i></a>
+                <a href="" className="btn btn-primary rounded-pill px-3 d-none d-lg-block">Join Us<i className="fa fa-arrow-right ms-3"></i></a>
             </div>
         </nav>
         </>
